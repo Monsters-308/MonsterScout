25fef664037fb05df6e5bf668e615836b407784f
AlexanderKaschta <alexander.kaschta@t-online.de>
2017-02-26 11:50:57 -0500

    Added random colors

---
+++ js/signin.js
@@ -0,0 +1,11 @@
+
+var colors = ["#57607c", "#1add9f", "#18d2ba", "#4894f0", "#b490f5", "#f45b93", "#ff1a4f", "#ff8463", "#ffbf00"];
+
+var random = Math.floor((Math.random() * 9) + 1); // max, min
+
+console.log(random);
+
+var button = document.getElementById("submitButton");
+
+button.style.backgroundColor = colors[random - 1];
+
